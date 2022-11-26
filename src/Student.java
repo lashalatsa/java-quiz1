df21121e4aca46c2a0a9b3f883f0032dfc4d2514
lashalatsa <73605080+lashalatsa@users.noreply.github.com>
2022-11-26 13:45:20 +0400

    Add files via upload

---
+++ src/Student.java
@@ -0,0 +1,45 @@
+//public class Student {
+//    private String saxeli;
+//    private String gvari;
+//    private int id;
+//
+//    public Student(String saxeli, String gvari, int id) {
+//        this.saxeli = saxeli;
+//        this.gvari = gvari;
+//        this.id = id;
+//    }
+//
+//
+//    public String getSaxeli() {
+//        return saxeli;
+//    }
+//
+//    public String getGvari() {
+//        return gvari;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setSaxeli(String saxeli) {
+//        this.saxeli = saxeli;
+//    }
+//
+//    public void setGvari(String gvari) {
+//        this.gvari = gvari;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Student{" +
+//                "saxeli='" + saxeli + '\'' +
+//                ", gvari='" + gvari + '\'' +
+//                ", id=" + id +
+//                '}';
+//    }
+//}
